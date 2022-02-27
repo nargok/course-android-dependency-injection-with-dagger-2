a70493ff7b97a59e1aa974515c7a3f74a440cb10
nargok <nargok@gmail.com>
2022-02-27 15:15:54 +0900

    re import base activity and base fragment

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.techyourchance.dagger2course.MyApplication
 import com.techyourchance.dagger2course.common.dependencyinjection.*
 import com.techyourchance.dagger2course.common.dependencyinjection.activity.ActivityModule
+import com.techyourchance.dagger2course.common.dependencyinjection.activity.DaggerActivityComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.app.AppModule
+import com.techyourchance.dagger2course.common.dependencyinjection.app.DaggerAppComponent
+import com.techyourchance.dagger2course.common.dependencyinjection.presentation.DaggerPresentationComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationModule
 
 open class BaseActivity: AppCompatActivity() {
--- app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/fragments/BaseFragment.kt
@@ -4,7 +4,10 @@ import androidx.fragment.app.Fragment
 import com.techyourchance.dagger2course.MyApplication
 import com.techyourchance.dagger2course.common.dependencyinjection.*
 import com.techyourchance.dagger2course.common.dependencyinjection.activity.ActivityModule
+import com.techyourchance.dagger2course.common.dependencyinjection.activity.DaggerActivityComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.app.AppModule
+import com.techyourchance.dagger2course.common.dependencyinjection.app.DaggerAppComponent
+import com.techyourchance.dagger2course.common.dependencyinjection.presentation.DaggerPresentationComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationModule
 import com.techyourchance.dagger2course.screens.common.activities.BaseActivity
 
