d97329a640ecf68fda5fa31c7c8935fe19d4ec56
nargok <nargok@gmail.com>
2022-02-27 14:50:33 +0900

    delete AppCompositionRoot

--- app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
+++ app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
@@ -1,13 +1,10 @@
 package com.techyourchance.dagger2course
 
 import android.app.Application
-import com.techyourchance.dagger2course.common.dependencyinjection.AppCompositionRoot
 
 class MyApplication : Application() {
-    lateinit var appCompositionRoot: AppCompositionRoot
 
     override fun onCreate() {
-        appCompositionRoot = AppCompositionRoot(this)
         super.onCreate()
     }
 
